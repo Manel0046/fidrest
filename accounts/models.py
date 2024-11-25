82f2a8e553e7cccec7632ca11758e0c566ca5e6b
Manel0046 <manel-guillen@hotmail.com>
2024-11-25 13:06:55 +0100

    Update models.py
    Creamos los modelos básicos para el sistema:

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+# Restaurante
+class Restaurant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+# Cliente
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+
+# Recompensas
+class Reward(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    points_required = models.IntegerField()
