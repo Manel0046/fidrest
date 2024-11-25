b072198c4a3f0ff7803727b3278641bd2b8af137
Manel0046 <manel-guillen@hotmail.com>
2024-11-25 13:16:15 +0100

    Update views.py
    Defino las primeras rutas para login, registro y consulta de puntos.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from .models import Customer, Restaurant
 
-# Create your views here.
+# Registro de usuarios
+def register(request):
+    if request.method == 'POST':
+        data = request.POST
+        user = User.objects.create_user(
+            username=data['username'],
+            password=data['password']
+        )
+        if data['type'] == 'restaurant':
+            Restaurant.objects.create(user=user, name=data['name'])
+        elif data['type'] == 'customer':
+            restaurant = Restaurant.objects.get(id=data['restaurant_id'])
+            Customer.objects.create(user=user, restaurant=restaurant)
+        return JsonResponse({'message': 'User registered successfully!'})
